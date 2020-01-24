a41b0679964d7145b99afd6a798dca0dfaf17738
Flavio Jr <flavioribeirojr@outlook.com>
2020-01-24 18:59:27 -0300

    Fix useScrollWatch useEffect dependency list

--- src/hooks/useScrollWatch.js
+++ src/hooks/useScrollWatch.js
@@ -2,26 +2,20 @@ import { useState, useEffect } from 'react';
 import { createScrollWatcher } from '../utils/scroll';
 
 function useScrollWatch() {
-  let ScrollWatcher = null;
+  const ScrollWatcher = createScrollWatcher();
   const [ verticalScroll, setVerticalScroll ] = useState(0);
 
   function onScroll() {
-    if (!ScrollWatcher) {
-      ScrollWatcher = require('../utils/scroll').createScrollWatcher();
-    }
-
     setVerticalScroll(ScrollWatcher.getVerticalOffset());
   }
 
   useEffect(function () {
-    ScrollWatcher = createScrollWatcher();
-
     document.addEventListener('scroll', onScroll);
 
     return function () {
       document.removeEventListener('scroll', onScroll);
     }
-  }, []);
+  });
 
   return verticalScroll;
 }
